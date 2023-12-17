15c2b55fd35d4088b89295e589e0317214032f89
mrmi <nikola.31023@gmail.com>
2023-12-17 03:20:00 +0100

    Create habit popup & habits redesign

--- lib/src/views/habit_list_screen.dart
+++ lib/src/views/habit_list_screen.dart
@@ -1,10 +1,18 @@
+import 'package:devfest_hackathon_2023/main.dart';
 import 'package:flutter/material.dart';
 import 'habit_details_screen.dart';
 import '../models/habit.dart';
 import '../utils/habit_data_handler.dart';
 
 class HabitListScreen extends StatefulWidget {
-  const HabitListScreen({super.key});
+  final Color primaryColor;
+  final Color accentColor;
+
+  const HabitListScreen({
+    Key? key,
+    required this.primaryColor,
+    required this.accentColor,
+  }) : super(key: key);
 
   @override
   State<HabitListScreen> createState() => _HabitListScreenState();
@@ -34,98 +42,187 @@ class _HabitListScreenState extends State<HabitListScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Material(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          // Create Habit form
-          Row(
-            children: [
-              Expanded(
-                child: TextField(
-                  controller: habitController,
-                  decoration: const InputDecoration(labelText: 'Enter Habit Name'),
-                ),
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  // Create a new habit and add it to the list
-                  if (habitController.text.isNotEmpty) {
-                    Habit newHabit = Habit(name: habitController.text);
-                    habits.add(newHabit);
-                    habitController.clear();
-                    setState(() {}); // Update the UI
-                  }
-                },
-                child: const Text('Create Habit'),
-              ),
-            ],
-          ),
-          const SizedBox(height: 20),
-          // Display list of habits
-          Expanded(
-            child: ListView.builder(
-              itemCount: habits.length,
-              itemBuilder: (context, index) {
-                return HabitListItem(
-                  habit: habits[index],
-                  onSaveCallback: () {
-                    saveData();
-                    // Update the state of the parent widget
-                    setState(() {});
-                  },
-                );
+    final Color primaryColor = widget.primaryColor; // Store primary color locally
+    final Color accentColor = widget.accentColor;
+    print("color: " + primaryColor.toString());
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Habits'),
+      ),
+      body: Container(
+        color: accentColor,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const SizedBox(height: 20),
+            ElevatedButton.icon(
+              onPressed: () {
+                _showCreateHabitDialog(context);
               },
+              icon: Icon(Icons.add, size: 36.0, color: accentColor),
+              label: Text(
+                'Create Habit',
+                style: TextStyle(fontSize: 18.0, color: accentColor),
+              ),
+              style: ElevatedButton.styleFrom(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                padding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 16.0),
+              ),
+            ),
+            const SizedBox(height: 20),
+            Expanded(
+              child: ListView.builder(
+                itemCount: habits.length,
+                itemBuilder: (context, index) {
+                  return HabitCard(
+                    habit: habits[index],
+                    onSaveCallback: () {
+                      saveData();
+                      setState(() {});
+                    },
+                    accentColor: accentColor,
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Future<void> _showCreateHabitDialog(BuildContext context) async {
+    TextEditingController habitController = TextEditingController();
+
+    return showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text("Create Habit"),
+          content: TextField(
+            controller: habitController,
+            decoration: InputDecoration(
+              labelText: 'Enter Habit Name',
             ),
           ),
-        ],
-      ),
+          actions: [
+            ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).pop(); // Close the dialog
+              },
+              child: Text("Cancel"),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                // Create a new habit and add it to the list
+                if (habitController.text.isNotEmpty) {
+                  Habit newHabit = Habit(name: habitController.text);
+                  habits.add(newHabit);
+                  habitController.clear();
+                  saveData();
+                  setState(() {}); // Update the UI
+                }
+                Navigator.of(context).pop(); // Close the dialog
+              },
+              child: Text("Confirm"),
+            ),
+          ],
+        );
+      },
     );
   }
 }
 
-class HabitListItem extends StatelessWidget {
+class HabitCard extends StatelessWidget {
   final Habit habit;
   final VoidCallback onSaveCallback;
+  final Color accentColor;
 
-  const HabitListItem({Key? key, required this.habit, required this.onSaveCallback}) : super(key: key);
+  const HabitCard({
+    Key? key,
+    required this.habit,
+    required this.onSaveCallback,
+    required this.accentColor,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     // Get the current date
     String currentDate = DateTime.now().toLocal().toString().split(' ')[0];
 
-    return ListTile(
-      title: Text(habit.name),
-      subtitle: Text('Streak Length: ${habit.streakLength}'),
-      trailing: Checkbox(
-        // Enable the checkbox only if it's not completed for today
-        onChanged: habit.isCompleted(currentDate) ? null : (bool? isChecked) {
-          // Mark the habit as completed if the checkbox is checked
-          if (isChecked != null) {
-            habit.markCompleted(isChecked);
-            // Save habits to SharedPreferences or update your state management as needed
-            onSaveCallback();
-            // Update the UI
-            ScaffoldMessenger.of(context).showSnackBar(
-              SnackBar(
-                content: Text(isChecked ? 'Habit marked as completed!' : 'Habit marked as not completed.'),
-              ),
-            );
-          }
-        },
-        // Set the checkbox state based on whether the habit is completed for today
-        value: habit.isCompleted(currentDate),
+    return Card(
+      elevation: 5,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(15),
       ),
-      onTap: () {
-        Navigator.push(
-          context,
-          MaterialPageRoute(
-            builder: (context) => HabitDetailsScreen(habit: habit),
+      child: InkWell(
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => HabitDetailsScreen(habit: habit),
+            ),
+          );
+        },
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Row(
+            children: [
+              Icon(
+                Icons.star,
+                size: 36.0,
+                color: habit.isCompleted(currentDate) ? Colors.amber : Colors.black, // Set the icon color
+              ),
+              const SizedBox(width: 16.0),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      habit.name,
+                      style: TextStyle(
+                        fontSize: 18.0,
+                        fontWeight: FontWeight.bold,
+                        color: accentColor
+                      ),
+                    ),
+                    const SizedBox(height: 8.0),
+                    Text(
+                      'Streak Length: ${habit.streakLength}',
+                      style: TextStyle(
+                        fontSize: 14.0,
+                        color: accentColor
+                      ),
+                    ),
+                    Checkbox(
+                      onChanged: habit.isCompleted(currentDate)
+                          ? null
+                          : (bool? isChecked) {
+                        if (isChecked != null) {
+                          habit.markCompleted(isChecked);
+                          onSaveCallback();
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            SnackBar(
+                              content: Text(isChecked
+                                  ? 'Habit marked as completed!'
+                                  : 'Habit marked as not completed.'),
+                            ),
+                          );
+                        }
+                      },
+                      value: habit.isCompleted(currentDate),
+                    ),
+                  ],
+                ),
+              ),
+            ],
           ),
-        );
-      },
+        ),
+      ),
     );
   }
 }
--- lib/src/views/hub.dart
+++ lib/src/views/hub.dart
@@ -1,14 +1,18 @@
+import 'package:flutter/material.dart';
 import 'package:devfest_hackathon_2023/src/services/quote_service.dart';
-import 'package:devfest_hackathon_2023/src/services/notification_service.dart';
 import 'package:devfest_hackathon_2023/src/views/habit_list_screen.dart';
 import 'package:devfest_hackathon_2023/src/views/quote_list_screen.dart';
 import 'package:devfest_hackathon_2023/src/views/self_examination_list_screen.dart';
-import 'package:flutter/material.dart';
 
 import 'meditation_screen.dart';
 
 class Hub extends StatelessWidget {
-  Hub({Key? key, required this.quoteService, required this.primaryColor, required this.accentColor}) : super(key: key);
+  const Hub({
+    Key? key,
+    required this.quoteService,
+    required this.primaryColor,
+    required this.accentColor,
+  }) : super(key: key);
 
   final QuoteService quoteService;
   final Color primaryColor;
@@ -17,54 +21,147 @@ class Hub extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+      appBar: AppBar(
+        title: Text('Hub', style: TextStyle(color: accentColor)),
+        backgroundColor: primaryColor,
+      ),
+      body: Container(
+        color: accentColor,
+        child: ListView(
+          padding: const EdgeInsets.all(16.0),
           children: [
-            ElevatedButton(
-              onPressed: () async {
+            HubButton(
+              onPressed: () {
                 Navigator.push(
                   context,
                   MaterialPageRoute(
-                      builder: (context) => QuoteListScreen(
-                          quoteService: quoteService,
+                    builder: (context) => QuoteListScreen(
+                      quoteService: quoteService,
                       primaryColor: primaryColor,
-                      accentColor: accentColor,)),
+                      accentColor: accentColor,
+                    ),
+                  ),
                 );
               },
-              child: const Text('QUOTES'),
+              label: 'QUOTES',
+              icon: Icons.format_quote,
+              backgroundColor: primaryColor,
+              textColor: accentColor,
+              description: 'Explore and discover meaningful quotes.',
             ),
-            ElevatedButton(
-              onPressed: () async {
+            HubButton(
+              onPressed: () {
                 Navigator.push(
                   context,
                   MaterialPageRoute(
-                      builder: (context) => const HabitListScreen()),
+                      builder: (context) => HabitListScreen(primaryColor: primaryColor, accentColor: accentColor)),
                 );
               },
-              child: const Text('HABITS'),
+              label: 'HABITS',
+              icon: Icons.star,
+              backgroundColor: primaryColor,
+              textColor: accentColor,
+              description: 'Track and build healthy habits.',
             ),
-            ElevatedButton(
-              onPressed: () async {
+            HubButton(
+              onPressed: () {
                 Navigator.push(
                   context,
                   MaterialPageRoute(
-                      builder: (context) => SelfExaminationListScreen()),
+                    builder: (context) => const SelfExaminationListScreen(),
+                  ),
                 );
               },
-              child: const Text('SELF EXAMINATION'),
+              label: 'SELF EXAMINATION',
+              icon: Icons.check,
+              backgroundColor: primaryColor,
+              textColor: accentColor,
+              description: 'Reflect and evaluate personal growth.',
             ),
-            ElevatedButton(
-              onPressed: () async {
+            HubButton(
+              onPressed: () {
                 Navigator.push(
                   context,
                   MaterialPageRoute(
-                      builder: (context) => const MeditationScreen()),
+                    builder: (context) => const MeditationScreen(),
+                  ),
                 );
               },
-              child: const Text('MEDITATION')
+              label: 'MEDITATION',
+              icon: Icons.spa,
+              backgroundColor: primaryColor,
+              textColor: accentColor,
+              description: 'Practice mindfulness and meditation.',
             ),
           ],
+        ),
+      ),
+    );
+  }
+}
+
+class HubButton extends StatelessWidget {
+  const HubButton({
+    Key? key,
+    required this.onPressed,
+    required this.label,
+    required this.icon,
+    required this.backgroundColor,
+    required this.textColor,
+    required this.description,
+  }) : super(key: key);
+
+  final VoidCallback onPressed;
+  final String label;
+  final IconData icon;
+  final Color backgroundColor;
+  final Color textColor;
+  final String description;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 5,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(15),
+      ),
+      child: InkWell(
+        onTap: onPressed,
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Row(
+            children: [
+              Icon(
+                icon,
+                size: 36.0,
+                color: textColor,
+              ),
+              const SizedBox(width: 16.0),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      label,
+                      style: TextStyle(
+                        fontSize: 18.0,
+                        color: textColor,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const SizedBox(height: 8.0),
+                    Text(
+                      description,
+                      style: TextStyle(
+                        fontSize: 14.0,
+                        color: Colors.black54,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
