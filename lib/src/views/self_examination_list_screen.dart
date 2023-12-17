5b9c1fa9742aba67267b6d37aff9d7179be0cffb
Damjan Vincic <damjanvincic2003@gmail.com>
2023-12-17 03:11:50 +0100

    Fix pixel overflow in self examination list screen

--- lib/src/views/self_examination_list_screen.dart
+++ lib/src/views/self_examination_list_screen.dart
@@ -74,145 +74,148 @@ class _SelfExaminationListScreenState extends State<SelfExaminationListScreen> {
       appBar: AppBar(
         title: const Text('SelfExamination Screen'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const Text('Stored Items:'),
-            Expanded(
-              child: ListView.builder(
-                shrinkWrap: true,
-                itemCount: items.length,
-                itemBuilder: (context, index) {
-                  return ListTile(
-                    title: Text('F: ${items[index].feeling}'),
-                    subtitle: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text('G: ${items[index].goals}'),
-                        Text('W: ${items[index].work}'),
-                        Text('D: ${items[index].date}'),
-                      ],
-                    ),
-                    trailing: IconButton(
-                      icon: const Icon(Icons.share),
-                      onPressed: () => _shareItem(items[index]),
-                    ),
-                  );
-                },
-              ),
-            ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () async {
-                // Pass the list to the input screen
-                await Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => SelfExaminationInputScreen(
-                      onDataChanged: saveData,
-                      existingItems: items,
-                    ),
-                  ),
-                );
-
-                // Reload the data after returning from the input screen
-                await loadData();
-                setState(() {});
-
-                // Save the updated list to shared preferences
-                await saveData();
-              },
-              child: const Text('Add New Item'),
-            ),
-            const SizedBox(height: 20),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Text('Show Line Chart'),
-                Switch(
-                  value: showLineChart,
-                  onChanged: (value) {
-                    setState(() {
-                      showLineChart = value;
-                    });
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text('Stored Items:'),
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.3,
+                child: ListView.builder(
+                  shrinkWrap: true,
+                  itemCount: items.length,
+                  itemBuilder: (context, index) {
+                    return ListTile(
+                      title: Text('F: ${items[index].feeling}'),
+                      subtitle: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text('G: ${items[index].goals}'),
+                          Text('W: ${items[index].work}'),
+                          Text('D: ${items[index].date}'),
+                        ],
+                      ),
+                      trailing: IconButton(
+                        icon: const Icon(Icons.share),
+                        onPressed: () => _shareItem(items[index]),
+                      ),
+                    );
                   },
                 ),
-                const Text('Show Pie Chart'),
-              ],
-            ),
-            const SizedBox(height: 20),
-            const Text('Feeling Over the Past 7 Days'),
-            SizedBox(
-              height: 300,
-              child: showLineChart ? LineChart(
-                LineChartData(
-                  titlesData: FlTitlesData(
-                    leftTitles: SideTitles(showTitles: true),
-                    bottomTitles: SideTitles(
-                      showTitles: true,
-                      reservedSize: 22,
-                      margin: 10,
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () async {
+                  // Pass the list to the input screen
+                  await Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => SelfExaminationInputScreen(
+                        onDataChanged: saveData,
+                        existingItems: items,
+                      ),
                     ),
+                  );
+        
+                  // Reload the data after returning from the input screen
+                  await loadData();
+                  setState(() {});
+        
+                  // Save the updated list to shared preferences
+                  await saveData();
+                },
+                child: const Text('Add New Item'),
+              ),
+              const SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text('Show Line Chart'),
+                  Switch(
+                    value: showLineChart,
+                    onChanged: (value) {
+                      setState(() {
+                        showLineChart = value;
+                      });
+                    },
                   ),
-                  borderData: FlBorderData(
-                    show: true,
-                    border: Border.all(
-                      color: const Color(0xff37434d),
-                      width: 1,
+                  const Text('Show Pie Chart'),
+                ],
+              ),
+              const SizedBox(height: 20),
+              const Text('Feeling Over the Past 7 Days'),
+              SizedBox(
+                height: 300,
+                child: showLineChart ? LineChart(
+                  LineChartData(
+                    titlesData: FlTitlesData(
+                      leftTitles: SideTitles(showTitles: true),
+                      bottomTitles: SideTitles(
+                        showTitles: true,
+                        reservedSize: 22,
+                        margin: 10,
+                      ),
                     ),
+                    borderData: FlBorderData(
+                      show: true,
+                      border: Border.all(
+                        color: const Color(0xff37434d),
+                        width: 1,
+                      ),
+                    ),
+                    minX: DateTime.now().subtract(const Duration(days: 7)).millisecondsSinceEpoch.toDouble(),
+                    maxX: DateTime.now().millisecondsSinceEpoch.toDouble(),
+                    minY: 0,
+                    maxY: 10,
+                    lineBarsData: [
+                      LineChartBarData(
+                        spots: generateSpotList(),
+                        isCurved: true,
+                        colors: [Colors.blue],
+                        dotData: FlDotData(show: false),
+                        belowBarData: BarAreaData(show: false),
+                      ),
+                    ],
                   ),
-                  minX: DateTime.now().subtract(const Duration(days: 7)).millisecondsSinceEpoch.toDouble(),
-                  maxX: DateTime.now().millisecondsSinceEpoch.toDouble(),
-                  minY: 0,
-                  maxY: 10,
-                  lineBarsData: [
-                    LineChartBarData(
-                      spots: generateSpotList(),
-                      isCurved: true,
-                      colors: [Colors.blue],
-                      dotData: FlDotData(show: false),
-                      belowBarData: BarAreaData(show: false),
+                ) : Stack(
+                  children: [
+                    PieChart(
+                      PieChartData(
+                        sections: List<PieChartSectionData>.generate(
+                          generatePieChartData().length,
+                              (index) {
+                            int feeling = generatePieChartData().keys.elementAt(index);
+                            double percentage = generatePieChartData()[feeling]!;
+                            int count = (percentage * items.length / 100).round();
+                            return PieChartSectionData(
+                              color: Colors.primaries[index % Colors.primaries.length],
+                              value: percentage,
+                              radius: percentage / 2,
+                              title: '$feeling: $count (${percentage.toStringAsFixed(1)}%)',
+                              titleStyle: const TextStyle(fontSize: 10),
+                            );
+                          },
+                        ),
+                        sectionsSpace: 0,
+                        centerSpaceRadius: 40,
+                        centerSpaceColor: Colors.white,
+                      ),
+                    ),
+                    Positioned(
+                      top: 8,
+                      left: 8,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: generateLegend(),
+                      ),
                     ),
                   ],
                 ),
-              ) : Stack(
-                children: [
-                  PieChart(
-                    PieChartData(
-                      sections: List<PieChartSectionData>.generate(
-                        generatePieChartData().length,
-                            (index) {
-                          int feeling = generatePieChartData().keys.elementAt(index);
-                          double percentage = generatePieChartData()[feeling]!;
-                          int count = (percentage * items.length / 100).round();
-                          return PieChartSectionData(
-                            color: Colors.primaries[index % Colors.primaries.length],
-                            value: percentage,
-                            radius: percentage / 2,
-                            title: '$feeling: $count (${percentage.toStringAsFixed(1)}%)',
-                            titleStyle: const TextStyle(fontSize: 10),
-                          );
-                        },
-                      ),
-                      sectionsSpace: 0,
-                      centerSpaceRadius: 40,
-                      centerSpaceColor: Colors.white,
-                    ),
-                  ),
-                  Positioned(
-                    top: 8,
-                    left: 8,
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: generateLegend(),
-                    ),
-                  ),
-                ],
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
