ad458e447881671251fde78d6a8954a473b903a7
mrmi <nikola.31023@gmail.com>
2024-02-20 15:04:36 +0100

    Platform check for load env variables

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'dart:async';
+import 'dart:io';
 import 'package:Actualizator/src/services/map_marker_service.dart';
 import 'package:Actualizator/src/screens/map_screen.dart';
 import 'package:Actualizator/src/screens/hub_screen.dart';
 import 'package:Actualizator/src/screens/settings_screen.dart';
 import 'package:Actualizator/src/services/quote_service.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_config/flutter_config.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:timezone/data/latest.dart' as tzdata;
 import 'src/services/notification_service.dart';
@@ -18,6 +20,9 @@ const Color accentColor = Colors.black87;
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  if (Platform.isAndroid || Platform.isIOS) {
+    await FlutterConfig.loadEnvVariables();
+  }
   await initializeNotifications();
   runApp(const MyApp());
 }
